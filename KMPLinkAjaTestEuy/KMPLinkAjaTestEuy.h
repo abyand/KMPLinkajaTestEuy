ad894b926966a76ad19c8a2b9d52f2f2d385d0b9
Abyan Dafa <abyand71@gmail.com>
2021-09-20 10:53:52 +0700

    Initial Commit

---
+++ KMPLinkAjaTestEuy/KMPLinkAjaTestEuy.h
@@ -0,0 +1,18 @@
+//
+//  KMPLinkAjaTestEuy.h
+//  KMPLinkAjaTestEuy
+//
+//  Created by Abyan Dafa on 20/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KMPLinkAjaTestEuy.
+FOUNDATION_EXPORT double KMPLinkAjaTestEuyVersionNumber;
+
+//! Project version string for KMPLinkAjaTestEuy.
+FOUNDATION_EXPORT const unsigned char KMPLinkAjaTestEuyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMPLinkAjaTestEuy/PublicHeader.h>
+
+
