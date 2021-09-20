c2a497429d3fc6de6c1b88d0a7d8d8a9bf717318
Abyan Dafa <abyand71@gmail.com>
2021-09-20 11:28:27 +0700

    add library and unit test

---
+++ KMPLinkAjaTestEuy/KMPLib.swift
@@ -0,0 +1,22 @@
+//
+//  KMPLib.swift
+//  KMPLinkAjaTestEuy
+//
+//  Created by Abyan Dafa on 20/09/21.
+//
+
+import Foundation
+
+public final class KMPLib {
+
+    let name = "KMPLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- KMPLinkAjaTestEuyTests/KMPLinkAjaTestEuyTests.swift
+++ KMPLinkAjaTestEuyTests/KMPLinkAjaTestEuyTests.swift
@@ -10,24 +10,16 @@ import XCTest
 
 class KMPLinkAjaTestEuyTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var swiftyLib: KMPLib!
+
+    override func setUp() {
+        swiftyLib = KMPLib()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSub() {
+        XCTAssertEqual(swiftyLib.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
